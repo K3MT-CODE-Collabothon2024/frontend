800e59eae7992e8c6e5e4d96ea9f55d33aa3d2a2
jaguel <m.jagiello03@gmail.com>
2024-10-19 09:21:31 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import './App.css';
-import AccountSummaryWidget from './widgets/AccountSummaryWidget';
-import AccountSummaryPopup from './widgets/AccountSummaryPopup';
-import BaseWidget from './widgets/BaseWidget';
 
 function App() {
   return (
     <div className="App">
-      {/* Dodanie BaseWidget, który zawiera AccountSummaryWidget jako content */}
-      <BaseWidget
-        isWide={false} // Ustawia szerokość widgetu
-        contentWidget={<AccountSummaryWidget />} // Wstawienie AccountSummaryWidget jako zawartość
-        contentPopup={<AccountSummaryPopup />}
-      />
+      
     </div>
   );
 }
