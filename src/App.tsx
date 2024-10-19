49322d83f7b61da14827c4ab47f8796c68191caa
Krystian Juszczyk <juszczyk-krystian@wp.pl>
2024-10-19 18:19:05 +0200

    Test infrastructure for layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React, { useState } from 'react';
-import './App.css';
+import "./App.css";
+import Layout from "./layout/Layout";
+import Header from "./widgets/Header";
 
 function App() {
+
+  const ids = [0,1,2,3,4,5];
+
   return (
-    <div className="App">
-    
+    <div className="App flex flex-col items-center justify-center">
+      <Header />
+      <div className="flex flex-col w-full  items-start justify-start px-10 py-4">
+        <h1 className="text-3xl font-bold">Dashboard</h1>
+        <p className="text-2xl text-gray-500">Welcome back, Jackson Jazzman</p>
+      </div>
+      <div className=" w-full items-center justify-center ">
+        <Layout ids={ids}/>
+       
+      </div>
     </div>
-    
   );
 }
 
