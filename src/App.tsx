dc9997dec3fe6fd0f000167920b03c978699580a
jaguel <m.jagiello03@gmail.com>
2024-10-19 13:33:20 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import React, { useState } from 'react';
 import './App.css';
-import BaseWidget from './widgets/BaseWidget';
-import AccountSummaryWidgetTight from './widgets/AccountSummary/AccountSummaryWidgetTight';
-import AccountSummaryPopup from './widgets/AccountSummary/AccountSummaryPopup';
-import AccountSummaryWidgetWide from './widgets/AccountSummary/AccountSummaryWidgetWide';
+
 function App() {
   return (
     <div className="App">
-      <BaseWidget
-        contentWidget={ <AccountSummaryWidgetWide/> }
-        contentPopup={ <AccountSummaryPopup/> }
-      />
+      
     </div>
   );
 }
