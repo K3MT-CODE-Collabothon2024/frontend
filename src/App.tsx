c98056eafda6d64c48e68b05d101500809970616
jaguel <m.jagiello03@gmail.com>
2024-10-19 21:21:48 +0200

    donut chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import './App.css';
+import BaseWidget from './widgets/BaseWidget';
+import AccountSummaryWidgetTight from './widgets/AccountSummary/AccountSummaryWidgetTight';
+import AccountSummaryPopup from './widgets/AccountSummary/AccountSummaryPopup';
+import AccountSummaryWidgetWide from './widgets/AccountSummary/AccountSummaryWidgetWide';
+import AccountAssets from './widgets/AccountSummary/AccountAssets';
 
 function App() {
   return (
     <div className="App">
-      
-
+      < BaseWidget contentWidget={<AccountAssets/>}/>
     </div>
   );
 }
--- src/widgets/BaseWidget.tsx
+++ src/widgets/BaseWidget.tsx
@@ -4,10 +4,9 @@ import closeIcon from '../icons/close_icon.png';
 
 interface BaseWidgetProps {
   contentWidget: React.ReactNode;
-  contentPopup: React.ReactNode;
 }
 
-const BaseWidget: React.FC<BaseWidgetProps> = ({ contentWidget, contentPopup }) => {
+const BaseWidget: React.FC<BaseWidgetProps> = ({ contentWidget }) => {
   const [isPopupVisible, setPopupVisible] = useState(false);
 
   const togglePopup = () => {
@@ -40,41 +39,8 @@ const BaseWidget: React.FC<BaseWidgetProps> = ({ contentWidget, contentPopup }) 
         </div>
       </motion.div>
 
-      {/* Pop-up */}
-      {isPopupVisible && (
-        <>
-          <div className="fixed inset-0 bg-gray-300 bg-opacity-75 z-40"></div>
-
-          <motion.div
-            className="fixed inset-0 flex justify-center items-center z-50"
-            initial={{ opacity: 0, scale: 0.8 }}
-            animate={{ opacity: 1, scale: 1 }}
-            exit={{ opacity: 0, scale: 0.8 }}
-            transition={{ duration: 0.5 }}
-          >
-            <motion.div
-
-              className="bg-commerzBrightGreen p-8 rounded-lg w-auto max-w-[90vw] max-h-[90vh] overflow-y-auto relative border-2 border-solid border-commerzBlue" 
-
-              initial={{ y: '100vh' }}
-              animate={{ y: 0 }}
-              exit={{ y: '100vh' }}
-              transition={{ type: 'spring', stiffness: 200, damping: 30 }}
-            >
-              <button
-                onClick={closePopup}
-                className="absolute top-0 right-0 w-30 h-30 text-commerzBlue p-2 flex items-center justify-center"
-              >
-                <img src={closeIcon} alt="Close" className="w-6 h-6 ml-2" />
-              </button>
-
-              <div className="flex flex-col justify-center items-center w-full">
-                {contentPopup}
-              </div>
-            </motion.div>
-          </motion.div>
-        </>
-      )}
+      
+  
     </div>
   );
 };
