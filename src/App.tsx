a0ce001c9327a6f12f35d25e4037902e241c2902
jaguel <m.jagiello03@gmail.com>
2024-10-20 09:53:56 +0200

    app correction

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import './App.css';
-import AccountAssets from './widgets/AccountSummaryWidget/AccountAssets';
-import IncomeExpensesChart from './widgets/AccountSummaryWidget/IncomeExpensesChart';
-import BaseWidget from './widgets/BaseWidget';
-import AccountAssetsSmaller from './widgets/AccountSummaryWidget/AccountAssetsSmaller';
-import IncomeExpensesChartSmaller from './widgets/AccountSummaryWidget/IncomeExpensesChartSmaller'
+
+import "./App.css";
+import Layout from "./layout/Layout";
+import Header from "./widgets/Header";
+import idData from "./data/currentWidgetIndexes.json";
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="App flex flex-col items-center justify-center">
+      <Header />
+      <div className="flex flex-col w-full  items-start justify-start px-10 py-4">
+        <h1 className="text-3xl font-bold">Dashboard</h1>
+        <p className="text-2xl text-gray-500">Welcome back, Jackson Jazzman</p>
+      </div>
+      <div className=" w-full items-center justify-center ">
+        <Layout ids={idData.ids} />
+      </div>
     </div>
   );
 }
